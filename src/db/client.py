e86142e0d231b4be909664ce17e76fe940258481
elphaim <vreys23@gmail.com>
2025-11-12 14:20:59 +0100

    Refactor database client functions for type hints

--- src/db/client.py
+++ src/db/client.py
@@ -1,10 +1,10 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine import Engine
+from sqlalchemy.exc import SQLAlchemyError
 
-def get_engine(db_url):
-  create_engine(db_url, echo=True)
-  pass
+def get_engine(db_url: str) -> Engine:
+  return create_engine(db_url, echo=True)
 
-def apply_schema(engine, schema_path):
-  with engine.connect() as conn:
-    conn.execute(text("schema_path"))
-    conn.commit()
+def apply_schema(engine: Engine, schema_path: str) -> None:
+  with engine.begin() as conn:
+    conn.execute(text(schema_path))
